135df9608c8461aac647c684e6e490515aab70d5
danbudris <d.c.budris@gmail.com>
2018-02-10 12:50:30 -0500

    Set up dependencies in Maven; set up string pipeline and word count mapper

--- src/main/java/edu/bu/cs755/Cleaner.java
+++ src/main/java/edu/bu/cs755/Cleaner.java
@@ -3,10 +3,9 @@ package edu.bu.cs755;
 import java.util.stream.*;
 import java.util.*;
 import java.util.function.Function;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.*;
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
-
 
 public class Cleaner {
     public static void main (String[] args) throws IOException {
@@ -14,22 +13,23 @@ public class Cleaner {
         S3ObjectInputStream s3is = pages.getObjectStream();
         List <String> pageStrings = IOUtils.readLines(s3is, "UTF-8");
 
-        //For each line in the list of pages, clean it, split it, and map it to a map with the word count as a value and the word as a key
-        Map <String, Long> cleanedList = pageStrings.stream()
+        //For each line in the list of pages, clean it of metadata tags, special characters and whitespace, can convert to uppercase
+        List <String> cleanedList = pageStrings.stream()
                 .map(s -> s.replaceAll("<[^>]+>"," "))
                 .map(s -> s.replaceAll("[^A-z\\s+]",""))
                 .map(String::toUpperCase)
-                .map(s -> s.split("\\s+"))
-                .flatMap(Arrays::stream)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                .collect(Collectors.toList());
 
-        //For each word/value pair in the cleaned list, sort it, get the top 5000, and map it back to a simple list of strings
-        List <String> sortedList = cleanedList.entrySet().stream()
+        //For each cleaned string (page), split it into words, then group the words by count, sort them by count, get the top 5000, and return just a list of the keys
+        List <String> sortedList = cleanedList.stream()
+                .map(s -> s.split("[\\s]+"))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(5000)
                 .map(x -> x.getKey())
                 .collect(Collectors.toList());
-
-        sortedList.forEach(System.out::println);
+        //sortedList.forEach(System.out::println);
     }
 }
