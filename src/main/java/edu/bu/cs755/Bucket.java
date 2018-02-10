8bc4bf2e8fded2549dcfd9b04d9044b64044ed2b
danbudris <d.c.budris@gmail.com>
2018-02-10 13:04:17 -0500

    Set up dependencies in Maven; set up string pipeline and word count mapper

--- src/main/java/edu/bu/cs755/Bucket.java
+++ src/main/java/edu/bu/cs755/Bucket.java
@@ -3,7 +3,7 @@ package edu.bu.cs755;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.*;
 
 public class Bucket {
     private String S3SourceBucket;
